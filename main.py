152d682892edaeba43aaab96f07f1b1b11700862
anilsapkota <muzicmaniac2015@gmail.com>
2023-10-13 15:43:35 -0500

    added a list to store all the instance of the class

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 class Item:
     pay_rate = 0.8 #the pay rate after 20% discount
-
+    all = []
     def __init__(self,name :str,price: float ,quantity=0):
         #Run validations to the received arguments
         assert price >=0, f"Price {price} is not greater than or equal to  zero!"
@@ -13,15 +13,23 @@ class Item:
         self.price = price
         self.quantity = quantity
 
+        #Actions to execute
+        Item.all.append(self)
+
     
     def calculate_total_price(self):
         return self.price * self.quantity
     
     def apply_discount(self):
-        self.price = self.price * Item.pay_rate
+        self.price = self.price * self.pay_rate
 
 
 item1 = Item("Phone",100,1)
-item1.apply_discount()
-print(item1.price)
+item2 = Item("Laptop",1000,3)
+item3 = Item("Cable",10,5)
+item4 = Item("Mouse",50,5)
+item5 = Item("Keyboard",75,5)
 
+
+for instance in Item.all:
+    print(instance.name)
