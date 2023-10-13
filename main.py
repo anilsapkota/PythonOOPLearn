00963476a12e3e213b64a64d2fbb0d825c3472cf
anilsapkota <muzicmaniac2015@gmail.com>
2023-10-13 13:07:39 -0500

    passed arguments in initializer

--- main.py
+++ main.py
@@ -1,28 +1,18 @@
 class Item:
 
-    def __init__(self,name,price,quantity):
+    def __init__(self,name,price,quantity=0):
         
         self.name = name
         self.price = price
         self.quantity = quantity
 
-    def calculate_total_price(self,x,y):
-        return x*y 
+    
+    def calculate_total_price(self):
+        return self.price * self.quantity
 
 
-item1 = Item("Phone",100,5)
-
-
-
-
-
-
+item1 = Item("Phone",100,1)
 item2 = Item("Laptop",1000,3)
 
-
-
-
-print(item1.name)
-print(item2.name)
-print(item1.price)
-print(item2.price)
+print(item1.calculate_total_price())
+print(item2.calculate_total_price())
