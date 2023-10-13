427dbb9430279e19180a352b62f8d95179b86912
anilsapkota <muzicmaniac2015@gmail.com>
2023-10-13 15:48:39 -0500

    repr added to represent

--- main.py
+++ main.py
@@ -22,6 +22,9 @@ class Item:
     
     def apply_discount(self):
         self.price = self.price * self.pay_rate
+    
+    def __repr__(self):
+        return f"Item('{self.name}',{self.price},{self.quantity})"
 
 
 item1 = Item("Phone",100,1)
@@ -31,5 +34,4 @@ item4 = Item("Mouse",50,5)
 item5 = Item("Keyboard",75,5)
 
 
-for instance in Item.all:
-    print(instance.name)
+print(Item.all)
