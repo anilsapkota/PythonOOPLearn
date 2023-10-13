e22476fadfd0b08a6e7ca7e57e5955078da44f1d
anilsapkota <muzicmaniac2015@gmail.com>
2023-10-13 13:16:06 -0500

    Added asset to validate the arguments

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 class Item:
 
-    def __init__(self,name,price,quantity=0):
-        
+    def __init__(self,name :str,price: float ,quantity=0):
+        #Run validations to the received arguments
+        assert price >=0, f"Price {price} is not greater than or equal to  zero!"
+        assert quantity >=0, f"Quantity {quantity} is not greater than or equal to zero!"
+
+
+
+        #assign to self object
         self.name = name
         self.price = price
         self.quantity = quantity
@@ -11,7 +17,7 @@ class Item:
         return self.price * self.quantity
 
 
-item1 = Item("Phone",100,1)
+item1 = Item("Phone",100,-1)
 item2 = Item("Laptop",1000,3)
 
 print(item1.calculate_total_price())
