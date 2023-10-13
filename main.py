6eaec02775d12e3f232ac88a5e863e63d5c56da4
anilsapkota <muzicmaniac2015@gmail.com>
2023-10-13 12:49:34 -0500

    basic methods and objects

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 class Item:
-    pass 
+    def calculate_total_price(self,x,y):
+        return x*y 
 
 
 item1 = Item()
@@ -8,5 +9,11 @@ item1.name = "Phone"
 item1.price = 100
 item1.quantity = 5 
 
-random_str = "aaa"
-print(random_str.upper())
+print(item1.calculate_total_price(item1.price, item1.quantity))
+
+item2 = Item()
+
+item2.name = "Laptop"
+item2.price = 1000
+item2.quantity = 3 
+print(item2.calculate_total_price(item2.price, item2.quantity))
